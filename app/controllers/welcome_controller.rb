119ef2282641975c2113e7bf94a02ca94581f48b
Dan Johnson <djboy71@gmail.com>
2015-02-06 21:29:12 +0000

    built out navigation and added welcome controller, set root

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+
+  def login
+  end
+end
